7c8b5767a609c0b298a22a6afac3803b4cccfe6e
BergeDios <santiago.goyret@gmail.com>
2022-05-20 19:08:27 +0000

    task 5 pep8 check, fix passing correct classes, working locally

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,18 +2,27 @@
 """module for index.py"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 
 @app_views.route("/status")
 def status():
     """method to reroute status page"""
     return jsonify(status="Ok")
 
+
 @app_views.route("/stats")
 def stats():
     """method that returns dictionary containing every class count"""
-    return jsonify(amenities=storage.count("Amenities"),
-                   cities=storage.count("City"),
-                   places=storage.count("Place"),
-                   reviews=storage.count("Review"),
-                   states=storage.count("State"),
-                   users=storage.count("User"))
+    return jsonify(amenities=storage.count(Amenity),
+                   cities=storage.count(City),
+                   places=storage.count(Place),
+                   reviews=storage.count(Review),
+                   states=storage.count(State),
+                   users=storage.count(User))
