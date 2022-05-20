d82cbbd4383a9511012b5cf4571eb32d7aae03da
BergeDios <santiago.goyret@gmail.com>
2022-05-20 19:21:16 +0000

    task 4 and 6 fix pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,18 +9,22 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """method to close a session"""
     storage.close()
+
 
 @app.errorhandler(404)
 def errorhandler(error):
     """method to handle 404 error"""
     return jsonify(error="Not found"), 404
 
+
 if __name__ == "__main__":
     if getenv('HBNB_API_HOST') and getenv('HBNB_API_PORT'):
-        app.run(host=getenv('HBNB_API_HOST'), port=getenv('HBNB_API_PORT'), threaded=True)
+        app.run(host=getenv('HBNB_API_HOST'),
+                port=getenv('HBNB_API_PORT'), threaded=True)
     else:
         app.run(host='0.0.0.0', port=5000, threaded=True)
