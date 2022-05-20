28005d414a987d0c9f7e7409affc778ba00655e1
BergeDios <santiago.goyret@gmail.com>
2022-05-20 15:18:42 +0000

    task 4 working locally, 6 locally and 5 needs test

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ module for api app"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,8 +14,13 @@ def teardown_db(exception):
     """method to close a session"""
     storage.close()
 
+@app.errorhandler(404)
+def errorhandler(error):
+    """method to handle 404 error"""
+    return jsonify(error="Not found"), 404
+
 if __name__ == "__main__":
-    if getenv(HBNB_API_HOST) and getenv(HBNB_API_PORT):
-        app.run(host=getenv(HBNB_API_HOST), port=getenv(HBNB_API_PORT), threaded=True)
+    if getenv('HBNB_API_HOST') and getenv('HBNB_API_PORT'):
+        app.run(host=getenv('HBNB_API_HOST'), port=getenv('HBNB_API_PORT'), threaded=True)
     else:
         app.run(host='0.0.0.0', port=5000, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,17 @@
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route('/status')
+@app_views.route("/status")
 def status():
     """method to reroute status page"""
-    return jsonify({'status': "Ok"})
+    return jsonify(status="Ok")
+
+@app_views.route("/stats")
+def stats():
+    """method that returns dictionary containing every class count"""
+    return jsonify(amenities=storage.count("Amenities"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
